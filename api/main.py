825cfa4b7a4d3595126dd24ec26b7d1aff6bedbf
palmal24 <malesicnet24@example.com>
2023-07-15 18:25:44 +0200

    swapped api call for frontend

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ import os
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 UNSPLASH_API = "https://api.unsplash.com/photos/random"
@@ -11,21 +12,21 @@ if not UNSPLASH_KEY:
     raise EnvironmentError("Unsplash key is missing...")
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
     headers = {
-        "Accept-Version": "v1",
+        "Accept-Version": "v1", 
         "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
+        }
     params = {"query": word}
     response = requests.get(url=UNSPLASH_API, headers=headers, params=params)
-    
+
     data = response.json()
-    return {"data": data}
+    return data
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5050, debug=False)
